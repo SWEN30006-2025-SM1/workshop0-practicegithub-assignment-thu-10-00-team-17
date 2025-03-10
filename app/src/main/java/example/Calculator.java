dcc8089689888803f5e94a24ecd473bb0ee6e53c
TolAndrew <andrew.tol@student.unimelb.edu.au>
2025-03-10 14:36:37 +1100

    Deleted redundant comments.

--- app/src/main/java/example/Calculator.java
+++ app/src/main/java/example/Calculator.java
@@ -2,12 +2,10 @@ package example;
 
 public class Calculator {
     public int add(int a, int b) {
-        // TODO: Implement this method
         return a+b;
     }
 
     public int minus(int a, int b) {
-        // TODO: Implement this method
         if (a > b) {
             return a-b;
         } else {
@@ -16,12 +14,10 @@ public class Calculator {
     }
 
     public int multiply(int a, int b) {
-        // TODO: Implement this method
         return a*b;
     }
 
     public double divide(int a, int b) {
-        // TODO: Implement this method
         if (b == 0) {
             throw new ArithmeticException("Cannot divided by 0");
         }
